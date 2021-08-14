c29244fe5437390b53ce8818388bbcc0d599172b
NetiFebriyani <88937526+NetiFebriyani@users.noreply.github.com>
2021-08-14 22:29:04 +0700

    Add files via upload

---
+++ home/admin.php
@@ -0,0 +1,38 @@
+<div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col">
+            <h1 class="m-0 text-dark">Dashboard</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+<div class="row">
+	<div class="col-lg-4 col-8">
+		<!-- small box -->
+		<div class="small-box bg-primary">
+			<div class="inner">
+				<p>Jumlah Pegawai</p>
+				<h3>3</h3>
+			</div>
+			<div class="icon">
+				<i class="ion ion-person-add"></i>
+			</div>
+			<a href="#"class="small-box-footer">
+			</a>
+		</div>
+	</div>
+	<div class="col-lg-4 col 8">
+		<!-- small box -->
+		<div class="small-box bg-warning">
+			<div class="inner">
+				<p>Pengguna Sistem</p>
+				<h3>2</h3>
+			</div>
+			<div class="icon">
+				<i class="ion ion-android-happy"></i>
+			</div>
+			<a href="#" class="small-box-footer">
+			</a>
+		</div>
+	</div>
